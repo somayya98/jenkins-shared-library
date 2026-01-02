b004e6c1362f9288d61251a6c2697602636c1832
somayya <somayya.adusumalli98@gmail.com>
2026-01-02 20:09:00 +0530

    jenkins

--- nodeJSEKSPipeline.groovy
+++ nodeJSEKSPipeline.groovy
@@ -132,22 +132,22 @@ def call (){
                     }
                 }
             }
-            stage('Trivy Scan'){
-                steps {
-                    script{
-                        sh """
-                            trivy image \
-                            --scanners vuln \
-                            --severity HIGH,CRITICAL,MEDIUM \
-                            --pkg-types os \
-                            --exit-code 1 \
-                            --format table \
-                            ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
-                        """
-                    }
-                }
-            }
-        }
+        //     stage('Trivy Scan'){
+        //         steps {
+        //             script{
+        //                 sh """
+        //                     trivy image \
+        //                     --scanners vuln \
+        //                     --severity HIGH,CRITICAL,MEDIUM \
+        //                     --pkg-types os \
+        //                     --exit-code 1 \
+        //                     --format table \
+        //                     ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
+        //                 """
+        //             }
+        //         }
+        //     }
+        // }
 
         post {
             always {
