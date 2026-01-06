d434139a362eb26fd998a7f3675df4b96b9daa10
somayya <somayya.adusumalli98@gmail.com>
2026-01-06 20:03:16 +0530

    jenkins

--- vars/EKSDeploy.groovy
+++ vars/EKSDeploy.groovy
@@ -12,17 +12,17 @@ def call (Map configMap){
             ACC_ID = "930832106480"
             PROJECT = configMap.get("project")
             COMPONENT = configMap.get("component")
-            deploy_to = configMap.get("deploy_to")
+            .deploy_to = configMap.get(".deploy_to")
             REGION = "us-east-1"
         }
         options {
             timeout(time: 30, unit: 'MINUTES') 
             disableConcurrentBuilds()
         }
-        // parameters {
-        //     string(name: 'appVersion', description: 'Which app version you want to deploy')
-        //     choice(name: 'deploy_to', choices: ['dev', 'qa', 'produ'], description: 'Pick something') 
-        // }
+        parameters {
+            string(name: 'appVersion', description: 'Which app version you want to deploy')
+            choice(name: '.deploy_to', choices: ['dev', 'qa', 'produ'], description: 'Pick something') 
+        }
         // This is build section
         stages {
             
@@ -32,10 +32,10 @@ def call (Map configMap){
                         withAWS(region:'us-east-1',credentials:'aws-creds') {
                             sh """
                                 set -e
-                                aws eks update-kubeconfig --region ${REGION} --name ${PROJECT}-${deploy_to}
+                                aws eks update-kubeconfig --region ${REGION} --name ${PROJECT}-${.deploy_to}
                                 kubectl get nodes
                                 sed -i "s/IMAGE_VERSION/${appVersion}/g" values.yaml
-                                helm upgrade --install ${COMPONENT} -f values-${deploy_to}.yaml -n ${PROJECT} --atomic --wait --timeout=5m .
+                                helm upgrade --install ${COMPONENT} -f values-${.deploy_to}.yaml -n ${PROJECT} --atomic --wait --timeout=5m .
                             """
                         }
                     }
@@ -43,7 +43,7 @@ def call (Map configMap){
             }
             stage('Functional Testing'){
                 when{
-                    expression { deploy_to == "dev" }
+                    expression { .deploy_to == "dev" }
                 }
                 steps{
                     script{
