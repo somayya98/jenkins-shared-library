1c70af2e0809eeb05b4c67ae68fe64c566fc5bc9
somayya <somayya.adusumalli98@gmail.com>
2026-01-06 16:50:12 +0530

    jenkins

--- vars/EKSDeploy.groovy
+++ vars/EKSDeploy.groovy
@@ -34,9 +34,9 @@ def call (Map configMap){
                                
                                 aws eks update-kubeconfig --region ${REGION} --name ${PROJECT}-${deploy_to}
                                 kubectl get nodes
-                                echo ${deploy_to}, ${appVersion}
-                                // sed -i "s/IMAGE_VERSION/${appVersion}/g" values.yaml
-                                // helm upgrade --install ${COMPONENT} -f values-${deploy_to}.yaml -n ${PROJECT} --atomic --wait --timeout=5m .
+                                
+                                sed -i "s/IMAGE_VERSION/${appVersion}/g" values.yaml
+                                helm upgrade --install ${COMPONENT} -f values-${deploy_to}.yaml -n ${PROJECT} --atomic --wait --timeout=5m .
                             """
                         }
                     }
